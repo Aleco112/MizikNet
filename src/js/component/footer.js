ab54d2f6b28d51e71e0a0ea2523d07369cffe848
Aleco112 <aleco112@yahoo.fr>
2022-06-28 02:41:55 +0000

    Monday update

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -3,7 +3,8 @@ import React, { Component } from "react";
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
-		By login, you agree to ther <a>Terms of service</a> and <a>Privacy Policy</a>
+		By login, you agree to ther <a className="text-primary">Terms of service</a> and <a className="text-primary">Privacy Policy</a>
 		</p>
 	</footer>
 );
+
