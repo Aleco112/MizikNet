d8cf0784cbd04db846fb54f75de853130cc9b280
Aleco112 <aleco112@yahoo.fr>
2022-09-08 01:40:21 +0000

    imaging resize

--- src/img/index.js
+++ src/img/index.js
@@ -1,7 +1,7 @@
 const IMAGES={
     appleStore:require('./applestore.png').default,
     eye:require('./eye-24.png').default,
-    eye2:require('./eye2-24.png').default,
+    eye2:require('./eye2-30.png').default,
     key:require('./key-24.png').default,
     mail:require('./mail-24.png').default,
     photo:require('./Photo.png').default,
