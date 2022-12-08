43a1eb41197d3722605cd2a94d692f93c111584a
Aleco112 <aleco112@yahoo.fr>
2022-12-08 03:14:16 +0000

    remove when done

---
+++ src/js/views/project.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function project() {
+  return (
+    <>
+    <div className='main' >Real Time Chart</div>
+    <form>
+       { /*first block*/}
+        <div>
+    <div> Best bid : itbit</div>
+
+    <div>
+    <div> bid price </div>
+    <div>Bid Quantity</div>
+    </div>
+    </div>
+    {/* Second Block */}
+    <div>
+    <div> Best Ask: Kraken</div>
+    <div>
+
+        <div>Ask Price</div>
+        <div>Ask Quantity</div>
+    </div>
+    </div>
+
+    </form>
+    </>
+  )
+}
+
+export default project
